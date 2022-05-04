662e027c0c8d4e9ce762646af62d1fb462e16b55
ss0g <55930267+ss0g@users.noreply.github.com>
2022-05-04 16:28:47 -0700

    33_degree exists now

--- generate_gif.py
+++ generate_gif.py
@@ -11,16 +11,10 @@ def generate_gif(duration):
 
     frames = []
     for i in range(45):
-        if i < 33: # 33_degree doesn't exist rn
-            frames.append(bg.copy())
-            ith_degree = Image.open(f"./high_quality_gif_files/gear/{i:02d}_degree.png")
-            ith_degree_rgba = ith_degree.convert("RGBA")
-            frames[i].paste(ith_degree_rgba, (0, 0), ith_degree_rgba)
-        elif i > 33:
-            frames.append(bg.copy())
-            ith_degree = Image.open(f"./high_quality_gif_files/gear/{i:02d}_degree.png")
-            ith_degree_rgba = ith_degree.convert("RGBA")
-            frames[i - 1].paste(ith_degree_rgba, (0, 0), ith_degree_rgba)
+        frames.append(bg.copy())
+        ith_degree = Image.open(f"./high_quality_gif_files/gear/{i:02d}_degree.png")
+        ith_degree_rgba = ith_degree.convert("RGBA")
+        frames[i].paste(ith_degree_rgba, (0, 0), ith_degree_rgba)
 
     frames[0].save("./high_quality_gif_files/gear.gif", save_all=True, append_images=frames[1:], duration=duration, loop=0)
     print("Generated gear.gif at ./high_quality_gif_files/gear.gif")
